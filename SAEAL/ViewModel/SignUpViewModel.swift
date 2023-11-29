7921e3508edb2d959d8123c575def5ced246e297
usingkim <kimyujin173@gmail.com>
2023-11-29 21:04:23 +0900

    structure: 폴더 구조 변경

---
+++ SAEAL/ViewModel/SignUpViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewModel.swift
+//  SAEAL
+//
+//  Created by 김유진 on 11/23/23.
+//
+
+import SwiftUI
+
+struct SignUpViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUpViewModel()
+}
